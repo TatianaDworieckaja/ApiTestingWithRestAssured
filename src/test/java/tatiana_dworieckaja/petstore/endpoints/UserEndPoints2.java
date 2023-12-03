d3542577a37be2f96062323f655d5be87fe4c3bc
tatianaItAcademy1 <t.dworieckaja@gmail.com>
2023-12-03 11:42:09 +0100

    clean up

---
+++ src/test/java/tatiana_dworieckaja/petstore/endpoints/UserEndPoints2.java
@@ -0,0 +1,56 @@
+package tatiana_dworieckaja.petstore.endpoints;
+
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import tatiana_dworieckaja.petstore.payload.User;
+
+import java.util.ResourceBundle;
+
+import static io.restassured.RestAssured.given;
+
+public class UserEndPoints2 {
+
+    static ResourceBundle getUrl(){
+        ResourceBundle routes = ResourceBundle.getBundle("routes");
+        return routes;
+    }
+
+    public static Response createUser(User payload) {
+
+        return given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(payload)
+                .when()
+                .post(getUrl().getString("post_url"));
+
+    }
+
+    public static Response getUser(String username) {
+        return given()
+                .accept(ContentType.JSON)
+                .pathParam("username", username)
+                .when()
+                .get(getUrl().getString("get_url"));
+    }
+
+    public static Response updateUser(String username, User payload){
+        return given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .pathParam("username", username)
+                .body(payload)
+                .when()
+                .put(getUrl().getString("update_url"));
+    }
+
+    public static Response deleteUser(String username){
+        return given()
+                .accept(ContentType.JSON)
+                .pathParam("username", username)
+                .when()
+                        .delete(getUrl().getString("delete_url"));
+    }
+
+}
---
+++ src/test/java/tatiana_dworieckaja/petstore/test/UserTests2.java
@@ -0,0 +1,77 @@
+package tatiana_dworieckaja.petstore.test;
+
+import com.github.javafaker.Faker;
+import io.restassured.response.Response;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import tatiana_dworieckaja.petstore.endpoints.UserEndPoints;
+import tatiana_dworieckaja.petstore.endpoints.UserEndPoints2;
+import tatiana_dworieckaja.petstore.payload.User;
+
+public class UserTests2 {
+    User userPayLoad;
+    Faker faker;
+    public Logger logger;
+
+    @BeforeClass
+    public void setup(){
+        faker = new Faker();
+        userPayLoad  = new User();
+
+        logger = LogManager.getLogger(this.getClass());
+
+        logger.info("******* Initial testdata is creating *******");
+
+        userPayLoad.setUsername(faker.name().username());
+        userPayLoad.setUserStatus(faker.number().numberBetween(0,1));
+        userPayLoad.setEmail(faker.internet().safeEmailAddress());
+        userPayLoad.setFirstName(faker.name().firstName());
+        userPayLoad.setId(faker.idNumber().hashCode());
+        userPayLoad.setLastName(faker.name().lastName());
+        userPayLoad.setPassword(faker.internet().password());
+        userPayLoad.setPhone(faker.phoneNumber().cellPhone());
+
+        logger.info("******* Initial testdata was created *******");
+    }
+
+    @Test(priority = 1)
+    public void testPostUser(){
+
+        logger.info("******* Creating user *******");
+        Response response = UserEndPoints2.createUser(userPayLoad);
+        response.then().log().all();
+        Assert.assertEquals(response.getStatusCode(), 200);
+        Assert.assertEquals(response.getContentType(), "application/json");
+        Assert.assertEquals((response.then().extract().path("type")), "unknown");
+        logger.info("******* User was created *******");
+    }
+
+    @Test (priority = 2)
+    public void testGetUserByName(){
+    logger.info("******* Reading user *******");
+    Response response = UserEndPoints2.getUser(userPayLoad.getUsername());
+    response.then().log().all();
+    Assert.assertEquals(response.getStatusCode(), 200);
+    logger.info("******* User info is displayed *******");
+    }
+
+    @Test(priority = 3)
+    public void testUpdateUser(){
+        logger.info("******* Updating user *******");
+        userPayLoad.setFirstName(faker.name().firstName());
+        Response response = UserEndPoints2.updateUser(userPayLoad.getFirstName(), userPayLoad );
+        response.then().log().body().statusCode(200);
+        logger.info("******* User was updated *******");
+    }
+
+    @Test(priority=4)
+    public void testDeleteUser(){
+        logger.info("******* Deleting user *******");
+        Response response = UserEndPoints2.deleteUser(userPayLoad.getUsername());
+        response.then().log().body().statusCode(200);
+        logger.info("******* User was deleted *******");
+    }
+}
